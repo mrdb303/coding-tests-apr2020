76af90977d1205ea76bee3f7aabb328639b15672
Danny Brien <dannybrien@ntlworld.com>
2020-04-15 16:16:54 +0100

    Truncated some code - used ES6 class syntax

--- js/fightinggame.js
+++ js/fightinggame.js
@@ -1,4 +1,3 @@
-
 // Converted from PHP to JavaScript.
 // Improved page design over first challenge.
 // Better colour scheme with flatter buttons.
@@ -36,11 +35,11 @@ const MAGINIFIED_INC_DEC_ON = true;
 
 
 
-function Creature(creatureSpecies, creatureName) {
-
+class Creature {
+	constructor() {
 	this.creatureValid = false;
-	this.passedInSpecies = creatureSpecies;
-	this.name = creatureName;
+	this.creatureSpecies = '';
+	this.name = '';
 	
 	this.creatureType ='';
 	this.image = '';
@@ -49,82 +48,32 @@ function Creature(creatureSpecies, creatureName) {
 	this.specialPower = '';
 	this.tablerowdata = [];
 	this.deleted = false;
-}
+	}
 
-Creature.prototype = {
-	constructor: Creature,
-	
-	setupCreature: function() {
-
-		this.creatureValid = true;
-
-		// These are the unique settings for each creature type. 
-		// Random generation of creature type is purposely decoupled 
-		// from the Creature class.
-
-		switch(this.passedInSpecies){
-			case('Witch'):
-				this.creatureType = this.passedInSpecies;
-				this.image = 'images/witch.png';
-				this.strength = this.getRandomInt({'min': 60, 'max': 80});
-				this.health = this.getRandomInt({'min': 50, 'max': 60})
-			break;
-			case('Dragon'):
-				this.creatureType = this.passedInSpecies;
-				this.image = 'images/dragon.png';
-				this.strength = this.getRandomInt({'min': 80, 'max': 90});
-				this.health = this.getRandomInt({'min': 80, 'max': 90})
-			break;
-			case('Snake'):
-				this.creatureType = this.passedInSpecies;
-				this.image = 'images/snake.png';
-				this.strength = this.getRandomInt({'min': 30, 'max': 60});
-				this.health = this.getRandomInt({'min': 30, 'max': 90})
-			break;
-			case('Troll'):
-				this.creatureType = this.passedInSpecies;
-				this.image = 'images/troll.png';
-				this.strength = this.getRandomInt({'min': 22, 'max': 65});
-				this.health = this.getRandomInt({'min': 60, 'max': 92})
-			break;
-			default:
-				this.creatureValid = false;
-				// This detects if an invalid creature type is supplied.
-				// Main creature creation loop checks this value and will
-				// only output the data to the game table if this 
-				// value is true
-		}
-
-		if(this.name === '') this.creatureValid = false;
-
-	},
-
-
-	getRandomInt: function(MinAndMaxValues) {
-		let min = Math.ceil(MinAndMaxValues.min);
-		let max = Math.floor(MinAndMaxValues.max);
+	getRandomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
 		return Math.floor(Math.random() * (max - min + 1)) + min;
-	},
+	}
 
-	deleteCreatureFromGame: function() {
+	deleteCreatureFromGame() {
 		this.deleted = true;
-	},
+	}
 
-	incrementHealth: function() {
+	incrementHealth() {
 		this.health++;
-	},
+	}
 
-	decrementHealth: function() {
+	decrementHealth() {
 		if(this.health !== 0) this.health--;
-	},
+	}
 
-	getHealthValue: function() {
+	getHealthValue() {
 		return this.health;
-	},
+	}
 
-
-	getDetailsAsArray: function() {
-		this.tablerowdata['creatureType'] = this.creatureType;
+	getDetailsAsArray() {
+		this.tablerowdata['creatureType'] = this.creatureSpecies;
 		this.tablerowdata['name'] = this.name;
 		this.tablerowdata['image'] = this.image; 
 		this.tablerowdata['strength'] = this.strength;
@@ -132,16 +81,61 @@ Creature.prototype = {
 		this.tablerowdata['specialPower'] = this.specialPower;
 		return this.tablerowdata;
 	}
-};
+}
 
+
+class Witch extends Creature{
+	constructor(name) {
+		super(name);
+		this.name = name;
+		this.creatureSpecies = "Witch";
+		this.image = 'images/witch.png';
+		this.strength = this.getRandomInt(60, 80);
+		this.health = this.getRandomInt(50,60);
+	}
+}
+
+class Dragon extends Creature{
+	constructor(name) {
+		super(name);
+		this.name = name;
+		this.creatureSpecies = "Dragon";
+		this.image = 'images/dragon.png';
+		this.strength = this.getRandomInt(80, 90);
+		this.health = this.getRandomInt(80, 90);
+	}
+}
+
+class Snake extends Creature{
+	constructor(name) {
+		super(name);
+		this.name = name;
+		this.creatureSpecies = "Snake";
+		this.image = 'images/snake.png';
+		this.strength = this.getRandomInt(30, 60);
+		this.health = this.getRandomInt( 30, 90);
+	}
+}
+
+class Troll extends Creature{
+	constructor(name) {
+		super(name);
+		this.name = name;
+		this.creatureSpecies = "Troll";
+		this.image = 'images/troll.png';
+		this.strength = this.getRandomInt(22, 65);
+		this.health = this.getRandomInt(60, 92);
+	}
+}
 
 
 // This class demonstrates level 4 - Storing the powers 
 // Note that the web page removes the output of the power
 // types from the screen when dice is re-rolled, but the
 // data is still retained.
-function Powers() {
+class Powers {
 
+	constructor() {
 	this.powers = [];
 	this.availablePowers = ['Increases health by 100',
 		'Decreases opponents health by 100',
@@ -149,24 +143,21 @@ function Powers() {
 		'Decreases strength between 1 - 100',
 		'Hides (Skips a go)',
 		'Steals 50% of the strength from the opponent'];
-}
+	}
 
-Powers.prototype = {
-	constructor: Powers,
-
-	setPowers: function(numberThrown) {
+	setPowers(numberThrown) {
 		this.powers = [];
 
 		for(let count=0; count<= numberThrown-1; count++){
 			this.powers.unshift(this.availablePowers[count]);
 		}
-	},
+	}
 
-	clearPowers: function() {
+	clearPowers() {
 		this.powers = [];
-	},
+	}
 
-	getPowers: function(){
+	getPowers(){
 		return this.powers;
 	}
 };
@@ -177,39 +168,47 @@ Powers.prototype = {
 // creature type.
 // After consideration, I was of the opinion that names should be
 // suited to creature types (i.e. Trolls with names indicating
-// little intelligence, witches with female names etc).
+// little intelligence, witches with female sounding names etc).
 // 
-// This tracks which creature names have 
-// already been issued to avoid the name being issued again.
+// This tracks which creature names have already been issued to avoid 
+// the name being issued again.
 // Should an extreme number of creatures be generated that exceeds
 // the number of names available, the names will wrap around and start
-// again. Also randomises name order for each creature type.
+// again. Also randomises name order for each creature type on refresh.
 // 
 // The name issuing functionality has been decoupled from the creature
-// creation class on purpose, although creature type name must be valid. 
+// creation classes on purpose, to allow for future import of name data 
+// from a JSON file and allow the flexibility to use just one master
+// list of names for all of the creature types, if this is what the 
+// next stages of the challenge requires.
 
-function CreatureNames(availableNames) {
+class CreatureNames {
+	// One single instance per creature type.
+
+	constructor(availableNames){
 
 	this.availableNames = availableNames;
 	this.creatureCountMax = availableNames.length;
-	this.creatureTypeArrIndex = 0;
+	this.creatureTypeArrIndex = 0; // tracks index num of name issued.
 	this.issuedName = '';
 	this.shuffleNames(this.availableNames);
-}
+	}
 
-CreatureNames.prototype = {
-	constructor: CreatureNames,
 
-	getName: function() {
+	getName() {
 
 		if(this.creatureTypeArrIndex === this.creatureCountMax) this.creatureTypeArrIndex = 0;
+		// if the current index number exceeds the number of creature 
+		// names in array, then reset the counter back to 0 for re-issuing
+		// names.
+
 		this.issuedName = this.availableNames[this.creatureTypeArrIndex];
 		this.creatureTypeArrIndex++;
 		
 		return this.issuedName;
-	},
+	}
 
-	shuffleNames: function (array) {
+	shuffleNames(array) {
 
 		// Durstenfeld shuffle - courtesy of Stackoverflow
 		for(let i = array.length - 1; i > 0; i--) {
@@ -271,7 +270,7 @@ const allWitchNames = ['Nelly', 'Esmerelda', 'Germintrude', 'Agnetha', 'Britney'
 	'Gruffella', 'Greasola', 'Sweatina', 'Esmerelda', 'Mungola', 
 	'Martha', 'Mersha', 'Putrina', 'Punga', 'Dungella', 
 	'Cruddenia', 'Cruddella', 'Nell', 'Krona', 
-	'Munter', 'Mutrid', 'Mavis', 'Croak', 'Belchette' ];
+	'Munter', 'Mutrid', 'Mavis', 'Croak', 'Belchette'];
 
 const allDragonNames = ['Morgon', 'Kalgon', 'Albert', 'Malvor', 'Pednor', 
 	'Jecoda', 'Magdor', 'Mengon', 'Hartador', 'Movor', 'Crandon', 'Torfon', 
@@ -311,14 +310,13 @@ let snakeNames = new CreatureNames(allSnakeNames);
 let trollNames = new CreatureNames(allTrollNames);
 
 
-
 let resetButton = document.getElementById('btn-reset').addEventListener('click', resetPage);
 let rollButton = document.getElementById('btn-roll').addEventListener('click', rollDice);
 
 
 // If number of creature types are added to, add the creature type to the
 // following array - also add the type to the creatureNameFactory and main
-// creature creation object.
+// creature type creation classes.
 let availableCreatures = ['Witch','Dragon','Snake','Troll'];
 
 let maxNumberOfCreaturesAvailable = availableCreatures.length;
@@ -338,8 +336,21 @@ for(let count=0; count<NUMBER_OF_CREATURES; count++){
 
 	creatureNameFound = creatureNameFactory(randomCreatureType);
 
-	creatureData[count] = new Creature(randomCreatureType, creatureNameFound );
-	creatureData[count].setupCreature();
+	switch(randomCreatureType) {
+		case("Witch"):
+			creatureData[count]= new Witch(creatureNameFound);
+		break;
+		case("Dragon"):
+			creatureData[count]= new Dragon(creatureNameFound);
+		break;
+		case("Snake"):
+			creatureData[count]= new Snake(creatureNameFound);
+		break;
+		case("Troll"):
+			creatureData[count]= new Troll(creatureNameFound);
+		break;
+	}
+
 }
 
 
@@ -347,7 +358,6 @@ let powers = new Powers();
 
 let tableHeadings = Array("Name","Type","Strength","Health", "Special Power", "Action");
 createDynamicTable('#table-box', tableHeadings, creatureData);
-
 
 
 
@@ -361,11 +371,11 @@ function createDynamicTable(cssIdName, tableHeadings, tableData){
 	
 	let table = document.createElement('table');
 
-	// Table headings
+	// Table headings:
 	let head = document.createElement('thead');
 	let cell;
 
-	tableHeadings.forEach(function(item, index) {
+	tableHeadings.forEach(function(item) {
 		cell = document.createElement('th');
 		cell.appendChild(document.createTextNode(item));
 		head.appendChild(cell);
@@ -374,35 +384,36 @@ function createDynamicTable(cssIdName, tableHeadings, tableData){
 	table.appendChild(head);
 
 
-	// Table data	
+	// Table data:
 	let indivRowData = [];
 	let body = document.createElement('tbody');
+	let creatureRow ;
 
 	tableData.forEach(function(item, index) {
 		if(item != null){
-			let row = document.createElement('tr');
-			row.id = "row" + index;
 			indivRowData = item.getDetailsAsArray();
-
-			cell = createTDElementText(indivRowData['name']);
-			row.appendChild(cell);  // name column (text)
-			cell = createTDElementImage(indivRowData['image'], indivRowData['creatureType']);
-			row.appendChild(cell);	// type column (image)
-			cell = createTDElementText(indivRowData['strength']);
-			row.appendChild(cell);	// strength column (text)
-			cell = createTDElementText(indivRowData['health']);
-			row.appendChild(cell);	// health column (text)
-			cell = createTDElementText(indivRowData['specialPower']);
-			row.appendChild(cell);	// special power column (text)
-			cell = createTDWithInputButtons(indivRowData, index);
-			row.appendChild(cell);	// action column (3 buttons)
-			body.appendChild(row);
-
+			creatureRow = createCreatureTableDataRow(indivRowData, index);
+			body.appendChild(creatureRow);
 		}
 	});
 
 	table.appendChild(body);
 	tablediv.appendChild(table);	
+}
+
+function createCreatureTableDataRow(indivRowData, index){
+	
+	let row = document.createElement('tr');
+	row.id = "row" + index;
+
+	row.appendChild(createTDElementText(indivRowData['name'])); 
+	row.appendChild(createTDElementImage(indivRowData['image'], indivRowData['creatureType']));
+	row.appendChild(createTDElementText(indivRowData['strength']));
+	row.appendChild(createTDElementText(indivRowData['health']));
+	row.appendChild(createTDElementText(indivRowData['specialPower']));
+	row.appendChild(createTDWithInputButtons(indivRowData, index));
+
+	return row;
 }
 
 function createTDElementText(textValue){
@@ -423,13 +434,10 @@ function createTDElementImage(imgPath, title){
 function createTDWithInputButtons(indivRowData, objcount) {
 	let tdCell = document.createElement('td');
 
-	let delButton = createTDElementInputButtons('images/delete.png', 'btn', 'del', objcount);
-	let incButton = createTDElementInputButtons('images/heart_inc.png', 'btn', 'inc', objcount);
-	let decButton = createTDElementInputButtons('images/heart_dec.png', 'btn', 'dec', objcount);
-
-	tdCell.appendChild(delButton);
-	tdCell.appendChild(incButton);
-	tdCell.appendChild(decButton);
+	// Buttons for creature actions
+	tdCell.appendChild(createTDElementInputButtons('images/delete.png', 'btn', 'del', objcount));
+	tdCell.appendChild(createTDElementInputButtons('images/heart_inc.png', 'btn', 'inc', objcount));
+	tdCell.appendChild(createTDElementInputButtons('images/heart_dec.png', 'btn', 'dec', objcount));
 
 	return tdCell;
 }
@@ -511,8 +519,8 @@ function rollDice(e){
 
 function changeDice(dice1, dice2, doubleDice){
 	let diceBox = document.getElementById("dice-box");
-	diceBox.getElementsByTagName("img")[0].src = "images/dice" + dice1 + ".png";
-	diceBox.getElementsByTagName("img")[1].src = "images/dice" + dice2 + ".png";
+	diceBox.getElementsByTagName("img")[0].src = `images/dice${dice1}.png`;
+	diceBox.getElementsByTagName("img")[1].src = `images/dice${dice2}.png`;
 	
 	let powersBox = document.getElementById("powers-box");
 	powersBox.innerHTML = '';
@@ -522,7 +530,7 @@ function changeDice(dice1, dice2, doubleDice){
 
 		let powerImageName;
 		(dice1 === 1)? powerImageName = "spower.png" : powerImageName = "spowers.png";
-		specialBox.getElementsByTagName("img")[0].src = "images/" + powerImageName;
+		specialBox.getElementsByTagName("img")[0].src = `images/${powerImageName}`;
 
 		powers.setPowers(dice1); // Only need to pass one value as it's a double value
 		OutputPowers();
@@ -538,9 +546,8 @@ function OutputPowers(){
 	let ulElem = document.createElement("ul");
 
 	let powerList = powers.getPowers();
-	let pListLength = powerList.length;
 
-	powerList.forEach(function(item, index, array){
+	powerList.forEach(function(item){
 		let para = document.createElement("li");
 		para.innerHTML = item;
 		ulElem.appendChild(para);
@@ -573,8 +580,8 @@ function changeRowValWithOptionalFontSizeChange(rowIDNumber, newHealth, ms, colN
 	rowSelected.getElementsByTagName("td")[3].innerHTML = newHealth;
 	if(MAGINIFIED_INC_DEC_ON){
 		rowSelected.getElementsByTagName("td")[3].style.fontSize = '2em';
-		setTimeout(function(){ rowSelected.getElementsByTagName("td")[colNum].style.fontSize = '0.938em';}, ms);
-		setTimeout(function(){ }, 60);
+		setTimeout(() => {rowSelected.getElementsByTagName("td")[colNum].style.fontSize = '0.938em';}, ms);
+		setTimeout(() => {}, 60);
 	}
 }
 
